25f00451cf174aa50b2c8fc22258ce2a7eaf7549
HaMzA <hamza.sabir72@yahoo.com>
2017-04-16 01:30:54 +0500

    Assignmennt

---
+++ 4.10.py
@@ -0,0 +1,10 @@
+MultiplesOf3=list(range(3,31,3))
+
+First3Multiple=MultiplesOf3[0:3]
+print(First3Multiple)
+
+MidMultiple=MultiplesOf3[3:7]
+print(MidMultiple)
+
+Last3Multiple=MultiplesOf3[7:10]
+print(Last3Multiple)
